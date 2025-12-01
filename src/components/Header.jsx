2ff807c304b9152a11d3aed792aa7e445d7cce72
guillaumebertil <guillaume.bertil@gmail.com>
2025-12-01 20:50:52 +0100

    Fix: fermeture du menu burger lors d'un clic sur un lien

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,46 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Header() {
+    const [ isOpen, setIsOpen ] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const closeMenu = () => {
+        setIsOpen(false);
+    };
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-5">
             {/* Logo */}
-            <Link className='navbar-brand text-light' to='/'>JOHN DOE</Link>
+            <Link className='navbar-brand text-light' to='/' onClick={closeMenu}>
+                JOHN DOE
+            </Link>
 
             {/* Menu burger */}
-            <button className='navbar-toggler bg-dark' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav'>
+            <button className='navbar-toggler' type='button' onClick={toggleMenu} aria-controls='navbarNav' aria-expanded={isOpen} aria-label='Toggle navigation'>
                 <span className='navbar-toggler-icon'></span>
             </button>
 
             {/* Menu de navigation */}
-            <div className='collapse navbar-collapse justify-content-end' id='navbarNav'>
-                <ul className='navbar-nav text-uppercase'>
+            <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id='navbarNav'>
+                <ul className='navbar-nav ms-auto text-uppercase'>
                     <li className='nav-item'>
-                        <Link to='/' className='nav-link text-light'>Home</Link>
+                        <Link to='/' className='nav-link text-light' onClick={closeMenu}>Home</Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to='/services' className='nav-link text-light'>Services</Link>
+                        <Link to='/services' className='nav-link text-light' onClick={closeMenu}>Services</Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to='/portfolio' className='nav-link text-light'>Portfolio</Link>
+                        <Link to='/portfolio' className='nav-link text-light' onClick={closeMenu}>Portfolio</Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to='/contact' className='nav-link text-light'>Contact</Link>
+                        <Link to='/contact' className='nav-link text-light' onClick={closeMenu}>Contact</Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to='/legalNotice' className='nav-link text-light'>Mentions Légales</Link>
+                        <Link to='/legalNotice' className='nav-link text-light' onClick={closeMenu}>Mentions Légales</Link>
                     </li>
                 </ul>
             </div>
