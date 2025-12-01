cd5f388c85c83313ce9b793f731a718cc79cc416
guillaumebertil <guillaume.bertil@gmail.com>
2025-12-01 20:13:51 +0100

    Fix: importer le JS de Bootstrap pour corriger le menu burger

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './styles/app.css';
