40f521c38d76cbe6e39a4660b6e8ad876d07af5f
guillaumebertil <guillaume.bertil@gmail.com>
2025-12-03 15:47:04 +0100

    feat: amélioration de la page Contact pour correspondre à la maquette

--- src/components/PagesTitle.jsx
+++ src/components/PagesTitle.jsx
@@ -1,7 +1,7 @@
 function PagesTitle(props) {
     return (
         <div className="container my-5">
-            <div className="col-md-4 mx-auto border-bottom border-4 border-primary">
+            <div className="col-md-6 col-sm-2 mx-auto border-bottom border-4 border-primary">
                 <h1 className="text-center">{props.title}</h1>
                 <p className="text-center mb-4 pb-2">{props.description}</p>
             </div>
